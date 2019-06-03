e890965d42194a9b6aff3f923cb2f6af1d540cbf
Alex Lopez <info@ithinkvirtual.com>
2019-06-03 10:36:26 -0400

    Update chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,12 +3,9 @@ $toolsDir	= "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName	= 'amazon-workspaces'
 $fileType	= 'msi'
 $url	= 'https://d2td7dqidlhjx7.cloudfront.net/prod/global/windows/Amazon+WorkSpaces.msi'
-$url64bit	= $url
 $softwareName	= "Amazon WorkSpaces*"
 $checksum	= 'FE56F0618E55F74D675C814B533B43B7B035CF301B8DD07FA7C9CE2480D199E1'
 $checksumType	= 'sha256'
-$checksum64	= $checksum
-$checksumType64	= $checksumType
 $silentArgs	= '/qn REBOOT=ReallySuppress'
 $validExitCodes		= @(0)
 
