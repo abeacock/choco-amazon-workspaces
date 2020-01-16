f14bd52ec7429d0b2dad2a298ffaff64914216f0
Alex Lopez <info@ithinkvirtual.com>
2020-01-16 15:56:18 -0500

    2.5.11 (#4)
    * Update README.md
    
    * Updated to 2.5.11

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageName	= 'amazon-workspaces'
 $fileType	= 'msi'
 $url	= 'https://d2td7dqidlhjx7.cloudfront.net/prod/global/windows/Amazon+WorkSpaces.msi'
 $softwareName	= "Amazon WorkSpaces*"
-$checksum	= '0499CF47BA913BF6946EB858A129B8EB2E37E2B3A8E4E3E7A733F3C33E675DC1'
+$checksum	= '0C75B3FE1CAFEC70B6D9F3422A45E50E5251A2C7FF501FD17A625AF512196C54'
 $checksumType	= 'sha256'
 $silentArgs	= '/qn REBOOT=ReallySuppress'
 $validExitCodes		= @(0)
