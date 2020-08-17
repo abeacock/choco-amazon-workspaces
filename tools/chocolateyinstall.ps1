b0ec42112bf10b78df4e2fe806201a86c3ab49d3
virtualex <info@ithinkvirtual.com>
2020-08-17 08:53:51 -0400

    Updated to 3.0.9

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageName	= 'amazon-workspaces'
 $fileType	= 'msi'
 $url	= 'https://d2td7dqidlhjx7.cloudfront.net/prod/global/windows/Amazon+WorkSpaces.msi'
 $softwareName	= "Amazon WorkSpaces*"
-$checksum	= '8F29CF689BDF9E2F800AAAF5E996A369FC761AC5A4AB45AEEC18DB91A1CA0F39'
+$checksum	= '9E3677F4181E2547B6F0E65AA316706307F091EEB6B9E10DD7773F32A9682061'
 $checksumType	= 'sha256'
 $silentArgs	= '/qn REBOOT=ReallySuppress'
 $validExitCodes		= @(0)
