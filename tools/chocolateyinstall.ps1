0e406f4d8f1793d0229101024637652a9ad894be
Beacock <andrew.beacock@theverygroup.com>
2020-10-01 12:02:29 +0100

    Updated to 3.0.10

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageName	= 'amazon-workspaces'
 $fileType	= 'msi'
 $url	= 'https://d2td7dqidlhjx7.cloudfront.net/prod/global/windows/Amazon+WorkSpaces.msi'
 $softwareName	= "Amazon WorkSpaces*"
-$checksum	= '9E3677F4181E2547B6F0E65AA316706307F091EEB6B9E10DD7773F32A9682061'
+$checksum	= '67DA5034BB031B1F94DEB8F6A5E68B88049068BB6D44462FEFCC2B1125B0B635'
 $checksumType	= 'sha256'
 $silentArgs	= '/qn REBOOT=ReallySuppress'
 $validExitCodes		= @(0)
